09ab047a649e9be3b57f1a6575ae4807901c4325
Mason <mason@pc-79-13.customer.ask4.lan>
2023-12-11 12:07:15 +0000

    working form?

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,20 +1,13 @@
 function Footer() {
     return (
         <footer id="App-Footer">
-            <div>
-                <h2>Contact Me</h2>
-                <form className="Contact-Form" netlify>
-                    <label htmlFor="Name">Name:</label>
-                    <input type="text" id="Name" aria-label="Name" placeholder="Name..." />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="Email" aria-label='Email' placeholder="Email..." />
-                    <label htmlFor="subject">Subject:</label>
-                    <input type='text' id='subject' placeholder='Subject...' />
-                    <label htmlFor="Body">Message:</label>
-                    <textarea id="Body" aria-label="Message" placeholder="Message..."/>
-                    <button type='submit'>Submit</button>
-                </form>
-            </div>
+            <h2>Contact Me</h2>
+            <form className="Contact-Form" method="POST" netlify >
+                <input type="text" id="Name" aria-label="Name" placeholder="Name..."/>
+                <input type="email" id="Email" aria-label='Email' placeholder="Email..."/>
+                <textarea id="Body" aria-label="Message" placeholder="Message..."/>
+                <button type='submit'>Submit</button>
+            </form>
         </footer>
     )
 }
