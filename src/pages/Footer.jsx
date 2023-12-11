89d8f93f60292423aced5874f1e421e3058ca9d3
Mason <mason@pc-79-13.customer.ask4.lan>
2023-12-11 12:18:56 +0000

    css for form

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,13 +1,18 @@
 function Footer() {
     return (
         <footer id="App-Footer">
-            <h2>Contact Me</h2>
-            <form className="Contact-Form" method="POST" netlify >
-                <input type="text" id="Name" aria-label="Name" placeholder="Name..."/>
-                <input type="email" id="Email" aria-label='Email' placeholder="Email..."/>
-                <textarea id="Body" aria-label="Message" placeholder="Message..."/>
-                <button type='submit'>Submit</button>
-            </form>
+            <div>
+                <h2>Contact Me</h2>
+                <form className="Contact-Form" netlify >
+                    <label htmlFor="Name">Name</label>
+                    <input type="text" id="Name" aria-label="Name" placeholder="Name..." />
+                    <label htmlFor="Email">Email</label>
+                    <input type="email" id="Email" aria-label='Email' placeholder="Email..." />
+                    <label htmlFor="Message">Message</label>
+                    <textarea id="Body" aria-label="Message" placeholder="Message..."/>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
         </footer>
     )
 }
