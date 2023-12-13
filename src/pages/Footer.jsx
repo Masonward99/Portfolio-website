983b3ae1ff097ae3f25dcfdfbec2a5109646d6e2
Mason <mason@pc-79-13.customer.ask4.lan>
2023-12-13 02:12:15 +0000

    added form html

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,13 +3,14 @@ function Footer() {
         <footer id="App-Footer">
             <div>
                 <h2>Contact Me</h2>
-                <form className="Contact-Form"   name="contact" method="POST" data-netlify='true' onSubmit='submit'>
+                <form className="Contact-Form" name="contact" method="POST" data-netlify="true" onSubmit="submit" action="/contact">
+                    <input type="hidden" name='form-name' value='content'/>
                     <label htmlFor="Name">Name</label>
-                    <input type="text" id="Name" aria-label="Name" placeholder="Name..." name='name'/>
+                    <input required type="text" id="Name" aria-label="Name" placeholder="Name..." name='name'/>
                     <label htmlFor="Email">Email</label>
-                    <input type="email" id="Email" aria-label='Email' placeholder="Email..." name='email'/>
+                    <input required type="email" id="Email" aria-label='Email' placeholder="Email..." name='email'/>
                     <label htmlFor="Message">Message</label>
-                    <textarea id="Body" aria-label="Message" placeholder="Message..." name='message'/>
+                    <textarea required id="Body" aria-label="Message" placeholder="Message..." name='message'/>
                     <button type='submit'>Submit</button>
                 </form >
             </div>
